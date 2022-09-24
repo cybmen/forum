544661abd246ea71a8d578ca32eba85cfb0097ab
cybmen <65395048+cybmen@users.noreply.github.com>
2022-09-25 00:19:54 +0400

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,140 @@
+$(document).ready(function () {
+  const textArray = textString.split("");
+  const textElem = $(".hacking-animation__text");
+
+  let count = 0;
+
+  setInterval(() => {
+    if (textArray[count] === "\n") {
+      textElem.append("<br>");
+    } else {
+      textElem.append(
+        `<span class="hacking-animation__character">${textArray[count]}</span>`
+      );
+    }
+
+    count++;
+    if (count === textArray.length) {
+      count = 0;
+    }
+  }, 20);
+});
+
+var textString = `.
+.
+/// D A R I U S ///
+    <project ENDLESS>:::MEF1ST0
+           Commence take control module RS232C.10.10.20
+           ...Connecting server N.A.O.M.I....
+...
+...
+//infected devices\\\\
+            ://remote control...
+            (**ACCESS GRANTED**)....
+                    message for victims="I'm inside! And you're my bitch now! HaHa FUCK YOU! Im DAR1US" ....
+                    ....
+                    ...
+                    ..
+                    .
+                    //STOP CALLING Victim...
+                    
+{David Fox}/// -- \\\\Marcus Klein\\\\
+  <M> Connecting Victim..:":":":
+  ff
+
+<Guardian Devil...>
+                /uploading system
+                
+                     N.A.O.M.I. ctOS
+                 Central Operating System 
+            highly advanced digital information 
+        system designed to manage the infrastructure 
+                         of city
+                -----I´m your future----
+                     klein404@ReSec;
+                     
+.
+.
+.
+                        START;;'DECRYPTION;;'PASSWORD
+LOAD...
+...
+   ...
+...
+   ...
+   ..
+   .
+$%$%$qazWSXedcRFVtgbYHNujmIKol$%$%$%
+      \\\\...SERVER ACCEPTED your CALL...
+      \\\\%^^attach{codemonkey}"'';
+
+///
+//
+
+/
+//
+////CODEMONKEYSENGAGE...&*&&*&;
+      [sd]
+       {hard like Shirt}
+
+ZXC..<>Ckfkd<>vkvodkskclc...
+                         { DARIUS3421D;CODE10011011101011
+ ..-=          
+ (B+ HACKING..//MYCITYKLATOVY )
+<\\\\boeie\\\\..www.klatovy.cz\\\\
+**(AR)12. (696); (96).ffkeoslk
+             0617513060 <<<<>OKAY
+             {FOntsize}:Hard.. FONTMANAGE 22 //
+             ((open duck)) //MONSTER_FRAME;
+                           (port Opened)
+                                (portisnotfound)// B1 }
+..
+..
+.....
+TARGET >> {ENTRACE GATE} break security
+--start decoding--
+1030239456982269034318345600639351403047216123412105266981337002327903235914921070246867430089854883804601929533958074506422692309
+0211191222230132910189949921339511452320154431348213235203921317640212136121883232991845110101234247969429700092122303071014820104
+2024320261902068751892096989492521230014300360322339163593422629461870051923311193080008327010910930242796335029347115881639216157
+4821943904631121423136002471712404257596303100930818893418223276623123092442129291852014302324721180295290210138533830193924235515
+72138115272520604305400226333499173271177002194000072134972301620113328318932310234098372468
+
+ 1GATE--20243202619020     BIND SUCCESSFUL 
+ ;;' /
+ 2GATE--72520604305,
+        6187005192331      BIND SUCCESSFUL                 
+ 0011011011101110110101101
+-----SCANNING-----
+//////////////////
+\\\\\\\\\\\\\\\\\\
+//////////////////
+\\\\\\\\\\\\\\\\\\
+//////////////////
+\\\\\\\\\\\\\\\\\\
+/////INPUT FOUND
+>>>>>>>>>>>>>>>>>>>>>>>   (     |    )  
+                          /     |    \\
+                         /     / \\    \\
+                         | D A R I U S  |
+                         |    inside    |
+/string found
+///BetterCheckYourDrawers
+....COPYING DATA TO THE SERVER
+
+>>GPS LOCATION
+>>PHONE CALLS
+>>TEXT MESSAGES
+>>EMAILS
+>>IDENTITY
+>>MEDIA
+>>OTHERS
+          ///(SHUTDOWN HACKING)
+             {WHIPEDRIVE}
+          whipe not found..
+S1293 K // P 430
+         N.A.O.M.I... VERSION 2.0
+...
+.....
+...
+....
+...`;
